3264ca4f9859d8b0ada36186bcfeb47bc449ae1b
mendela andrew <jurgeon018@gmail.com>
2020-11-21 14:35:46 +0200

    automatic updating commit

--- README.md
+++ README.md
@@ -1,5 +1,51 @@
+Change cart item quantity (using item.id)
+POST "/api/cart_item/<id>/item/"
+id - item.id 
+
+Get all items in cart 
+GET /api/cart_items/
+
+Create cart item
+POST /api/cart_items/
+item_id: item.id 
+quantity: int
+
+Delete all cart items.
+DELETE /api/cart_items/
+
+Get cart item 
+GET "/api/cart_item/<id>/"
+id - cart_item.id
+
+Change cart item quantity (using cart_item.id)
+PATCH "/api/cart_item/<id>/"
+quantity: int 
+id - cart_item.id
+
+Delete cart Item
+DELETE "/api/cart_item/<id>/"
+
+Get all items in favour 
+GET "/api/favour_items/"
+
+Add item to favours 
+POST "/api/favour_items/"
+item_id: item.id 
+
+Delete favour item
+DELETE "/api/favour_items/<id>/"
+id - favour_item.id 
+
+Remove item from favours.
+DELETE "/api/favour_item/<id>/remove_by_like/"
+id - item.id 
+
+Add favour item to cart.
+POST "/api/favour_item/<id>/add_to_cart/" 
+id - favour_item.id
+
+Add all favour items to cart. 
+POST "/api/add_favours_to_cart/"
 
 
 
-box requirements
-<!-- sw_cart  -->
--- api/urls.py
+++ api/urls.py
@@ -1,32 +1,8 @@
 from django.urls import path, include 
 from .views import * 
 
-# from rest_framework.routers import DefaultRouter
-
-# router = DefaultRouter()
-# router.register('cart_items', CartItemViewSet)
-
-
-old_urlpatterns = [
-  # path('clear_cart/',             clear_cart,              name='clear_cart'),
-  # path('get_cart_items/',         get_cart_items,          name='get_cart_items'),
-  # path('add_cart_item/',          add_cart_item,           name='add_cart_item'),
-  # path('remove_cart_item/',       remove_cart_item,        name='remove_cart_item'),
-  # path('change_cart_item_amount/',change_cart_item_amount, name='change_cart_item_amount'),
-  # path('change_item_amount/',     change_item_amount,      name='change_item_amount'),
-
-  # path('get_favours_amount/',     get_favours_amount,      name='get_favours_amount'),
-  # path('get_favours/',            get_favours,             name='get_favours'),
-  # path('add_favour/',             add_favour,              name='add_favour'),
-  # path('remove_favour/',          remove_favour,           name='remove_favour'),
-  # path('remove_favour_by_like/',  remove_favour_by_like,   name='remove_favour_by_like'),
-  # path('add_favour_to_cart/',     add_favour_to_cart,      name='add_favour_to_cart'),
-  # path('add_favours_to_cart/',    add_favours_to_cart,     name='add_favours_to_cart'),
-]
 
 urlpatterns = [
-  # path('', include(old_urlpatterns)), 
-  # path('', include(router.urls)),
   path('cart_items/',          cart_items,         name='cart_items'),
   path('cart_item/<id>/',      cart_item,          name='cart_item'),
   path('cart_item/<id>/item/', change_item_amount, name='change_item_amount'),
